bf8b64376ed675f34f9e25c9964cb3199e0918e7
배진호 <72592302+BaeJinho4028@users.noreply.github.com>
2024-06-28 03:52:16 +0900

    refactor: 1차 피드백 반영

--- src/main/java/com/example/memo/memo/service/RestTemplateService.java
+++ src/main/java/com/example/memo/memo/service/RestTemplateService.java
@@ -5,10 +5,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import com.example.memo.memo.service.exception.AiResponseErrorHandler;
+import com.example.memo.memo.service.models.AiSaveResponse;
 import com.example.memo.memo.service.models.AiSearchResponse;
 import com.example.memo.memo.service.models.MemoRequestBridge;
-import com.example.memo.memo.service.models.AiSaveResponse;
 
 import lombok.RequiredArgsConstructor;
 
@@ -22,18 +21,17 @@ public class RestTemplateService {
     private String aiUrl;
 
     public AiSaveResponse getTags(MemoRequestBridge memoRequestBridge) {
-        restTemplate.setErrorHandler(new AiResponseErrorHandler());
-        final String query = "/add_memo/";
-        final String uri = String.format("%s%s", aiUrl, query);
-        ResponseEntity<AiSaveResponse> aiResponse = restTemplate.postForEntity(uri, memoRequestBridge,
-            AiSaveResponse.class);
+        final String uri = aiUrl + "/add_memo/";
+        ResponseEntity<AiSaveResponse> aiResponse = restTemplate.postForEntity(
+            uri,
+            memoRequestBridge,
+            AiSaveResponse.class
+        );
         return aiResponse.getBody();
     }
 
     public AiSearchResponse searchMemo(String content) {
-        restTemplate.setErrorHandler(new AiResponseErrorHandler());
-        final String query = "/user_query?query=";
-        final String uri = String.format("%s%s%s", aiUrl, query, content);
+        final String uri = aiUrl + "/user_query?query=" + content;
         ResponseEntity<AiSearchResponse> aiResponse = restTemplate.getForEntity(uri, AiSearchResponse.class);
         return aiResponse.getBody();
     }
