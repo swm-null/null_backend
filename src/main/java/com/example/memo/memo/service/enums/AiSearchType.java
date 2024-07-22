76250d055ef93ba4f4399dd4d0dc1ad5006c6baa
Baejinho4028 <onionring4028@naver.com>
2024-07-23 00:13:30 +0900

    fix: json enum 직렬화 처리

--- src/main/java/com/example/memo/memo/service/enums/AiSearchType.java
+++ src/main/java/com/example/memo/memo/service/enums/AiSearchType.java
@@ -1,17 +1,32 @@
 package com.example.memo.memo.service.enums;
 
-import lombok.Getter;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
 
-@Getter
+import java.util.Arrays;
+
 public enum AiSearchType {
     SIMILARITY("similarity"),
     REGEX("regex"),
     TAG("tag"),
-    ;
+    UNSPECIFIED("unspecified");
 
-    private final String type;
+    private final String value;
 
-    AiSearchType(String type) {
-        this.type = type.toLowerCase();
+    AiSearchType(String value) {
+        this.value = value;
+    }
+
+    @JsonValue
+    public String getValue() {
+        return value;
+    }
+
+    @JsonCreator
+    public static AiSearchType forValue(String value) {
+        return Arrays.stream(AiSearchType.values())
+            .filter(type -> type.value.equals(value))
+            .findFirst()
+            .orElseThrow(() -> new IllegalArgumentException("Unknown enum type " + value));
     }
 }
