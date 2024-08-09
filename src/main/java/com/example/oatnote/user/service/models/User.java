e1d359aa272993ca28b4331474002d9fe90858c5
Baejinho4028 <onionring4028@naver.com>
2024-08-09 18:36:40 +0900

    feat: 유저 컬렉션 조건 추가

--- src/main/java/com/example/oatnote/user/service/models/User.java
+++ src/main/java/com/example/oatnote/user/service/models/User.java
@@ -3,6 +3,7 @@ package com.example.oatnote.user.service.models;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 
 @Document(collection = "users")
@@ -12,8 +13,10 @@ public class User {
     @Id
     private String id;
 
+    @NotBlank
     private String email;
 
+    @NotBlank
     private String password;
 
     public User(String email, String password) {
