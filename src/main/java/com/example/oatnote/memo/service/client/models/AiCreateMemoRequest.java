51502e9aa0285164cba42a08e6ad6550f75557e9
Baejinho4028 <onionring4028@naver.com>
2024-08-11 01:23:06 +0900

    fix: timestamp 추가

--- src/main/java/com/example/oatnote/memo/service/client/models/AiCreateMemoRequest.java
+++ src/main/java/com/example/oatnote/memo/service/client/models/AiCreateMemoRequest.java
@@ -3,6 +3,8 @@ package com.example.oatnote.memo.service.client.models;
 import static com.fasterxml.jackson.databind.PropertyNamingStrategies.SnakeCaseStrategy;
 import static io.swagger.v3.oas.annotations.media.Schema.RequiredMode.REQUIRED;
 
+import java.time.LocalDateTime;
+
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -11,7 +13,9 @@ import jakarta.validation.constraints.NotBlank;
 @JsonNaming(SnakeCaseStrategy.class)
 public record AiCreateMemoRequest(
     @NotBlank(message = "내용은 비워둘 수 없습니다.")
-    String content
+    String content,
+
+    LocalDateTime timestamp
 ) {
 
 }
--- src/main/java/com/example/oatnote/memo/service/client/models/AiCreateMemoResponse.java
+++ src/main/java/com/example/oatnote/memo/service/client/models/AiCreateMemoResponse.java
@@ -2,6 +2,7 @@ package com.example.oatnote.memo.service.client.models;
 
 import static com.fasterxml.jackson.databind.PropertyNamingStrategies.SnakeCaseStrategy;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
@@ -17,13 +18,14 @@ public record AiCreateMemoResponse(
 
     List<String> existingTagIds,
 
-    List<InnerTag> newTags
+    List<InnerTag> newTags,
+
+    LocalDateTime timestamp
 ) {
 
     public record InnerTag(
         String id,
         String name,
-        int depth,
         String parent,
         List<Double> embedding
     ) {
