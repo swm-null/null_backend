1cffa863fa59e6d627a3a4f53d759f7d910bbcd0
Baejinho4028 <onionring4028@naver.com>
2024-08-12 05:31:17 +0900

    chore: 불필요한 함수 제거

--- src/main/java/com/example/oatnote/memo/service/tag/TagRepository.java
+++ src/main/java/com/example/oatnote/memo/service/tag/TagRepository.java
@@ -8,7 +8,5 @@ import com.example.oatnote.memo.service.tag.models.Tag;
 
 public interface TagRepository extends MongoRepository<Tag, String> {
 
-    // List<Tag> findByDepth(int depth);
-
     List<Tag> findByParentTagIdIsNull();
 }
