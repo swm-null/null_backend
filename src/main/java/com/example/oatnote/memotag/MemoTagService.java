ee68357d53d9c2bffa16faeac8b744acc6a1f3fa
Baejinho4028 <onionring4028@naver.com>
2024-10-02 18:23:03 +0900

    chore: 줄넘김 수정

--- src/main/java/com/example/oatnote/memotag/MemoTagService.java
+++ src/main/java/com/example/oatnote/memotag/MemoTagService.java
@@ -221,8 +221,7 @@ public class MemoTagService {
             rawMemo,
             userId
         );
-        AICreateStructureResponse aiCreateStructureResponse = aiMemoTagClient.createStructure(
-            aiCreateStructureRequest);
+        AICreateStructureResponse aiCreateStructureResponse = aiMemoTagClient.createStructure(aiCreateStructureRequest);
         processMemoTag(aiCreateStructureResponse, rawMemo, userId, now);
     }
 
