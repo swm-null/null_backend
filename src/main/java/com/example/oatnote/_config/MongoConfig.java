4317e7da4ac28c5460f4fb335dcd3e7ce5052ea0
Baejinho4028 <onionring4028@naver.com>
2024-10-02 18:17:33 +0900

    chore: 디렉토리 위치 변경

--- src/main/java/com/example/oatnote/_config/MongoConfig.java
+++ src/main/java/com/example/oatnote/_config/MongoConfig.java
@@ -1,7 +1,6 @@
 package com.example.oatnote._config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
 @Configuration
 public class MongoConfig {
