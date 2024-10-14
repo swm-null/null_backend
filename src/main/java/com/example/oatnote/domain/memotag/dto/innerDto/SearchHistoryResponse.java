91a406f0b4928f66014cad4c7f29fd334b390a25
Baejinho4028 <onionring4028@naver.com>
2024-10-14 16:02:39 +0900

    chore: json snake case 수정

--- src/main/java/com/example/oatnote/domain/memotag/dto/innerDto/SearchHistoryResponse.java
+++ src/main/java/com/example/oatnote/domain/memotag/dto/innerDto/SearchHistoryResponse.java
@@ -1,10 +1,14 @@
 package com.example.oatnote.domain.memotag.dto.innerDto;
 
+import static com.fasterxml.jackson.databind.PropertyNamingStrategies.SnakeCaseStrategy;
+
 import java.time.LocalDateTime;
 
 import com.example.oatnote.domain.memotag.dto.SearchMemosResponse;
 import com.example.oatnote.domain.memotag.service.searchhistory.model.SearchHistory;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 
+@JsonNaming(SnakeCaseStrategy.class)
 public record SearchHistoryResponse(
     String query,
     LocalDateTime createdAt,
