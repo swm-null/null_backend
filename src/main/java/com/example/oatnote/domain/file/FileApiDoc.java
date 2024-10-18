222952b814a3b836a605102a33a13dbe556af8d3
배진호 <onionring4028@gmail.com>
2024-10-18 16:57:19 +0900

    Null 457 feature file type validation (#49)
    * feat: 파일 형식 검증 커스텀 애너테이션 추가
    
    * feat: 커스텀 애너테이션 적용
    
    * feat: 검증로직 수정 및 적용
    
    * refactor: 파일 형식 수정

--- src/main/java/com/example/oatnote/domain/file/FileApiDoc.java
+++ src/main/java/com/example/oatnote/domain/file/FileApiDoc.java
@@ -1,17 +1,20 @@
 package com.example.oatnote.domain.file;
 
+import static com.example.oatnote.web.validation.enums.AllowedFileTypeEnum.*;
+
 import java.util.List;
 
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.example.oatnote.domain.file.dto.UploadFileResponse;
 import com.example.oatnote.domain.file.dto.UploadFilesResponse;
+import com.example.oatnote.web.validation.AllowedFileType;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -21,6 +24,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
 @Tag(name = "Upload File", description = "파일 업로드 API")
+@Validated
 public interface FileApiDoc {
 
     @ApiResponses(
@@ -38,7 +42,7 @@ public interface FileApiDoc {
         produces = MediaType.APPLICATION_JSON_VALUE
     )
     ResponseEntity<UploadFileResponse> uploadFile(
-        @RequestPart MultipartFile file,
+        @RequestPart @AllowedFileType({IMAGE, AUDIO, TXT, SVG}) MultipartFile file,
         @AuthenticationPrincipal String userId
     );
 
@@ -57,7 +61,7 @@ public interface FileApiDoc {
         produces = MediaType.APPLICATION_JSON_VALUE
     )
     ResponseEntity<UploadFilesResponse> uploadFiles(
-        @RequestPart List<MultipartFile> files,
+        @RequestPart @AllowedFileType({IMAGE, AUDIO, TXT, SVG}) List<MultipartFile> files,
         @AuthenticationPrincipal String userId
     );
 }
