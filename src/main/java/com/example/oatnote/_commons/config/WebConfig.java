a8eb0ae679f6e0d44038798ef7cabfa24b32509a
Baejinho4028 <onionring4028@naver.com>
2024-10-31 16:16:32 +0900

    fix: cors properties 수정

--- src/main/java/com/example/oatnote/_commons/config/WebConfig.java
+++ src/main/java/com/example/oatnote/_commons/config/WebConfig.java
@@ -1,5 +1,6 @@
 package com.example.oatnote._commons.config;
 
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -8,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 
 @Configuration
 @RequiredArgsConstructor
+@EnableConfigurationProperties(CorsProperties.class)
 public class WebConfig implements WebMvcConfigurer {
 
     private final CorsProperties corsProperties;
