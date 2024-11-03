2f6534f24ba369df6f984216f629b017138b08cf
Baejinho4028 <onionring4028@naver.com>
2024-11-03 14:44:20 +0900

    temp

---
+++ src/main/java/com/example/oatnote/_commons/config/AsyncConfig.java
@@ -0,0 +1,25 @@
+package com.example.oatnote._commons.config;
+
+import java.util.concurrent.Executor;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+@Configuration
+@EnableAsync
+public class AsyncConfig {
+
+    @Bean(name = "AsyncMemoTagExecutor")
+    public Executor asyncExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(10);
+        executor.setMaxPoolSize(20);
+        executor.setQueueCapacity(200);
+        executor.setThreadNamePrefix("AsyncMemoTagThread-");
+        executor.initialize();
+        return executor;
+    }
+}
+
---
+++ src/main/java/com/example/oatnote/domain/memotag/service/AsyncMemoTagService.java
@@ -0,0 +1,12 @@
+package com.example.oatnote.domain.memotag.service;
+
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class AsyncMemoTagService {
+
+
+}
