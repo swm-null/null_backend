3f9c85ea8e5efa22e4003f29797438ae01956805
Baejinho4028 <onionring4028@naver.com>
2024-11-20 01:54:37 +0900

    fix: 오류 해결

--- src/main/java/com/example/oatnote/_commons/config/RedisConfig.java
+++ src/main/java/com/example/oatnote/_commons/config/RedisConfig.java
@@ -4,10 +4,10 @@ import org.redisson.api.RedissonClient;
 import org.redisson.spring.data.connection.RedissonConnectionFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
 import org.springframework.data.redis.listener.ChannelTopic;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.beans.factory.annotation.Value;
@@ -21,10 +21,10 @@ public class RedisConfig {
     private String topic;
 
     @Bean
-    public RedisMessageListenerContainer container(RedissonClient redissonClient, MessageListenerAdapter listenerAdapter) {
+    public RedisMessageListenerContainer container(RedisConnectionFactory connectionFactory, MessageListenerAdapter listenerAdapter, ChannelTopic topic) {
         RedisMessageListenerContainer container = new RedisMessageListenerContainer();
-        container.setConnectionFactory(new RedissonConnectionFactory(redissonClient));
-        container.addMessageListener(listenerAdapter, new ChannelTopic(topic));
+        container.setConnectionFactory(connectionFactory);
+        container.addMessageListener(listenerAdapter, topic);
         return container;
     }
 
@@ -39,11 +39,16 @@ public class RedisConfig {
     }
 
     @Bean
-    public RedisTemplate<String, Object> redisTemplate(RedissonClient redissonClient) {
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
-        template.setConnectionFactory(new RedissonConnectionFactory(redissonClient));
+        template.setConnectionFactory(connectionFactory);
         template.setKeySerializer(new StringRedisSerializer());
         template.setValueSerializer(new StringRedisSerializer());
         return template;
+    }
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory(RedissonClient redissonClient) {
+        return new RedissonConnectionFactory(redissonClient);
     }
 }
--- src/test/java/com/example/oatnote/OatNoteApplicationTests.java
+++ src/test/java/com/example/oatnote/OatNoteApplicationTests.java
@@ -5,13 +5,14 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.redisson.api.RedissonClient;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@SpringBootTest
-class OatNoteApplicationTests {
+import com.example.oatnote.config.TestConfig;
 
-    @MockBean
-    private RedissonClient redissonClient;
+@SpringBootTest
+@ContextConfiguration(classes = {TestConfig.class})
+class OatNoteApplicationTests {
 
     @Test
     void contextLoads() {
