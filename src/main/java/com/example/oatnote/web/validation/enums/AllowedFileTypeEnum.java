dd25f87c91c33cacf2d137163efa2f22b40a681e
Baejinho4028 <onionring4028@naver.com>
2024-11-22 02:17:49 +0900

    fix: 확장자 체크 수정

--- src/main/java/com/example/oatnote/web/validation/enums/AllowedFileTypeEnum.java
+++ src/main/java/com/example/oatnote/web/validation/enums/AllowedFileTypeEnum.java
@@ -1,7 +1,6 @@
 package com.example.oatnote.web.validation.enums;
 
-import java.util.List;
-import java.util.Map;
+import java.util.Set;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -9,29 +8,10 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum AllowedFileTypeEnum {
-    IMAGE(Map.of(
-        "jpg", List.of("image/jpeg"),
-        "jpeg", List.of("image/jpeg"),
-        "png", List.of("image/png"),
-        "webp", List.of("image/webp"),
-        "gif", List.of("image/gif")
-    )),
-    VOICE(Map.of(
-        "mp3", List.of("audio/mpeg"),
-        "mpeg", List.of("audio/mpeg"),
-        "mpga", List.of("audio/mpeg"),
-        "m4a", List.of("audio/m4a"),
-        "wav", List.of("audio/wav"),
-        "webm", List.of("audio/webm", "video/webm"),
-        "mp4", List.of("video/mp4"),
-        "mkv", List.of("video/x-matroska")
-    )),
-    TXT(Map.of(
-        "txt", List.of("text/plain")
-    )),
-    CSV(Map.of(
-        "csv", List.of("text/csv")
-    ));
+    IMAGE(Set.of("jpg", "jpeg", "png", "webp", "gif")),
+    VOICE(Set.of("mp3", "mpeg", "mpga", "m4a", "wav", "webm")),
+    TXT(Set.of("txt")),
+    CSV(Set.of("csv"));
 
-    private final Map<String, List<String>> fileTypes;
+    private final Set<String> extensions;
 }
